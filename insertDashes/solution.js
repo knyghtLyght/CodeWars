2959ca3892c2cd91d8b00cd36f0329431035e798
Kevin Farrow <kevnaf@gmail.com>
2018-02-15 12:50:09 -0800

    done

--- insertDashes/solution.js
+++ insertDashes/solution.js
@@ -1,11 +1,15 @@
 function insertDash(num) {
   let numArray = num.toString().split('');
+  let ansArray = [];
   for(let i = 0; i < numArray.length; i++) {
-    if(!(numArray[i] % 2 === 0)) {
-      if(!(numArray[i + 1] % 2 === 0)) {
-        numArray.splice(i + 1, 0,'-');
+    if(!(numArray[i] % 2 === 0) && !(numArray[i + 1] % 2 === 0)) {
+      ansArray.push(numArray[i]);
+      if(i < numArray.length - 1) {
+        ansArray.push('-');
       }
+    } else {
+      ansArray.push(numArray[i]);
     }
   }
-  return numArray.join('');
+  return ansArray.join('');
 }
