2e3dc780441caaa7ccfb3ea6913ce231ff269c47
Kevin <kevnaf@gmail.com>
2018-03-01 20:06:44 -0800

    I think the test are broke

--- niceArray/solution.js
+++ niceArray/solution.js
@@ -1,9 +1,13 @@
 function isNice(arr){
-  let flag = true;
-  arr.forEach(element => {
-    for(let i in arr) {
-      if(!(element + 1 === arr[i]) && !(element - 1 === arr[i])) flag = false;
+  let flag = false;
+  console.log(arr);
+  for(let i in arr) {
+    arr[i] = parseInt(arr[i]);
+    console.log(arr[i]);
+    for(let j in arr) {
+      if(arr[i] - 1 === arr[j] || arr[i] + 1 === arr[j]) flag = true;
+      console.log(arr[i] - 1, arr[i] + 1), arr[j];
     }
-  });
-  return flag;Thanks
+  }
+  return flag;
 }
